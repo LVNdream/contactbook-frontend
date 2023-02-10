c144c14f0f41b43096421223c3f7c41d52d7ee08
nhutb1910275 <nhutb1910275@student.ctu.edu.vn>
2023-02-10 11:37:14 +0700

    hien thi danh sach contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.css';
+// them thu vien bootstrap va fontawesome
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createApp(App).mount('#app')
+
+//them thuvien cua router
+import router from "./router";
+//createApp(App).mount('#app');
+
+createApp(App)
+.use(router).mount("#app");
